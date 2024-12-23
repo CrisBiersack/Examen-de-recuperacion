4d923643399158fa4106df15d3fdabf91e718f62
CrisBiersack <152736978+CrisBiersack@users.noreply.github.com>
2024-12-23 16:39:07 -0500

    Tercero

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("formulario-contacto").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Formulario enviado correctamente. ¡Gracias por contactarnos!");
+});
