7481f33f283a185ad3c0ed5d5d86b01da82e902c
CrisBiersack <152736978+CrisBiersack@users.noreply.github.com>
2024-12-23 21:34:30 -0500

    primero

--- js/boton.js
+++ js/boton.js
@@ -3,15 +3,15 @@ const nextButton = document.querySelector('.next-btn');
 const prevButton = document.querySelector('.prev-btn');
 
 let scrollPosition = 0;
-const cardWidth = document.querySelector('.program-card').offsetWidth + 15; // Ancho de la tarjeta + margen
+const cardWidth = document.querySelector('.program-card').offsetWidth + 15; 
 
 nextButton.addEventListener('click', () => {
   const maxScroll = carousel.scrollWidth - carousel.offsetWidth;
-  scrollPosition = Math.min(scrollPosition + cardWidth * 3, maxScroll); // Avanza 3 tarjetas
+  scrollPosition = Math.min(scrollPosition + cardWidth * 3, maxScroll); 
   carousel.style.transform = `translateX(-${scrollPosition}px)`;
 });
 
 prevButton.addEventListener('click', () => {
-  scrollPosition = Math.max(scrollPosition - cardWidth * 3, 0); // Retrocede 3 tarjetas
+  scrollPosition = Math.max(scrollPosition - cardWidth * 3, 0); 
   carousel.style.transform = `translateX(-${scrollPosition}px)`;
 });
